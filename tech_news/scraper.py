52d40c5c65ba6f9edefdc1d8653441670e6600c6
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-01-20 23:40:27 -0300

    feat: req1 - fetch of an url to scrape page info

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,20 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        time.sleep(1)
+        response = requests.get(
+            url, headers={"user-agent": "Fake user-agent"}, timeout=3
+        )
+        if (response.status_code == 200):
+            return response.text
+        else:
+            return None
+    except requests.exceptions.RequestException:
+        return None
 
 
 # Requisito 2
