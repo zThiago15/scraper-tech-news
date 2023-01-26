47a27debe6f11239ca7face5b9135daebe199f4c
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-01-26 14:24:46 -0300

    feat: req7 - searching news by date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
@@ -7,8 +8,14 @@ def search_by_title(title):
 
 
 # Requisito 7
-def search_by_date(date):
-    """Seu código deve vir aqui"""
+def search_by_date(date: str):
+    try:
+        formated_date = datetime(date).strftime('%d/%m/%y')
+        query = {'timestamp': {'$eq': formated_date}}
+        return [(new['title'], new['url']) for new in search_news(query)]
+
+    except ValueError:
+        raise ValueError('Data inválida')
 
 
 # Requisito 8
