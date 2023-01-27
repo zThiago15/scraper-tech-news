18604ad1cd5cbbe76d7f10ed44685192acfcc91d
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-01-27 02:06:44 -0300

    fix: returning of search_by_title and formated_date of search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -4,13 +4,14 @@ from datetime import datetime
 
 # Requisito 6
 def search_by_title(title):
-    return search_news(title)
+    query = {'title', {'$regex': title, '$options': 'i'}}
+    return [(new["title"], new["url"]) for new in search_news(query)]
 
 
 # Requisito 7
 def search_by_date(date: str):
     try:
-        formated_date = datetime(date).strftime('%d/%m/%y')
+        formated_date = datetime.fromisoformat(date).strftime('%d/%m/%y')
         query = {'timestamp': {'$eq': formated_date}}
         return [(new['title'], new['url']) for new in search_news(query)]
 
