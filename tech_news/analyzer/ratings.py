94a06a95e108b53110e9e3fcd70c11f63015f2d6
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-01-27 02:02:09 -0300

    feat: req10 - returning 5 top  news by comments

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,26 @@
+from tech_news.database import find_news
+from collections import Counter
+
+
+def sort_by_comment(e):
+    return e["comments_count"]
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+
+    # news = find_news()
+    # five_news = news.sort(key=e['comments_count'], reverse=True)
+    # return [(new['title'], new['url']) for new in five_news]
+    news = find_news()
+    news.sort(key=sort_by_comment, reverse=True)
+    if len(news) > 5:
+        news = news[:5]
+    list_formated = []
+    for news in news:
+        list_formated.append((news["title"], news["url"]))
+    return list_formated
 
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
